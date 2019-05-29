35a420e44cd3d72989f6f27f0deb9331255f8ff5
zheyu <zheyus89483@gmail.com>
2019-05-29 17:37:18 +0800

    0529

--- src/js/effect-transform.js
+++ src/js/effect-transform.js
@@ -1,4 +1,7 @@
 $(function () {
+
+    var $item_img = $('.m-carousel__item');
+
     $(window).on('mousemove mousein', function (e) {
 
         var str = 10;
@@ -13,36 +16,33 @@ $(function () {
         var $js_KV = $('#js-kv');
 
         if (isActive_KV) {
-            // $('#js-kv.-active').find('.-front').css('transform', 'translate3d('+ moveY * str / -6 + 'px, ' + moveX * str / 6 + 'px, ' + str / 0.6 + 'vw) rotateX(' + moveX * str / -6 + 'deg) rotateY(' + moveY * str / 6 + 'deg)')
-            // // $('#js-kv').find('.m-kv__layer.-back').css('transform', 'translateY(' + moveX * str / 6 + 'px) translateX(' + moveY * str / -6 + 'px) rotateX(' + moveX * str / 6 * -1 + 'deg) rotateY(' + moveY * str / 6 + 'deg) translateZ(' + str / 10 + 'vw)')
 
-            $js_KV.find('.-front').css('transform', 'translate(' + moveY * str / -6 + 'px,' + moveX * str / 6 + 'px) rotateX(' + moveX * str / 6 * -1 + 'deg) rotateY(' + moveY * str / 6 + 'deg) translateZ(' + str / 0.6 + 'vw)')
+            var tranX = moveY * str / -6;
+            var tranY = moveX * str / 1;
+            var rotaX = moveX * str / -6;
+            var rotaY = moveY * str / 6;
 
-            $js_KV.find('.m-ch-separation__img.-back.-ch-c').css('transform', 'translate(' + moveY * str / -6 + 'px,' + moveX * str / 6 + 'px) rotateX(' + moveX * str / 6 * -1 + 'deg) rotateY(' + moveY * str / 6 + 'deg) translateZ(' + str / 0.9 + 'vw)')
-            $js_KV.find('.m-ch-separation__img.-back.-ch-y').css('transform', 'translate(' + moveY * str / -6 + 'px,' + moveX * str / 6 + 'px) rotateX(' + moveX * str / 6 * -1 + 'deg) rotateY(' + moveY * str / 6 + 'deg) translateZ(' + str / 1.2 + 'vw)')
-            $js_KV.find('.m-ch-separation__img.-back.-ch-m').css('transform', 'translate(' + moveY * str / -6 + 'px,' + moveX * str / 6 + 'px) rotateX(' + moveX * str / 6 * -1 + 'deg) rotateY(' + moveY * str / 6 + 'deg) translateZ(' + str / 1.5 + 'vw)')
+            $js_KV.find('.-front').css('transform', 'translate(' + tranX + 'px,' + tranY + 'px) rotateX(' + rotaX + 'deg) rotateY(' + rotaY + 'deg) translateZ(' + str / 0.6 + 'vw)')
+
+            $js_KV.find('.m-ch-separation__img.-back.-ch-c').css('transform', 'translate(' + tranX + 'px,' + tranY + 'px) rotateX(' + rotaX + 'deg) rotateY(' + rotaY + 'deg) translateZ(' + str / 0.9 + 'vw)')
+            $js_KV.find('.m-ch-separation__img.-back.-ch-y').css('transform', 'translate(' + tranX + 'px,' + tranY + 'px) rotateX(' + rotaX + 'deg) rotateY(' + rotaY + 'deg) translateZ(' + str / 1.2 + 'vw)')
+            $js_KV.find('.m-ch-separation__img.-back.-ch-m').css('transform', 'translate(' + tranX + 'px,' + tranY + 'px) rotateX(' + rotaX + 'deg) rotateY(' + rotaY + 'deg) translateZ(' + str / 1.5 + 'vw)')
 
 
 
         } else {
             $js_KV.find('.m-ch-separation__img').removeAttr('style')
 
-
-            var imgRotate = 0;
-            imgRotate = (moveY * str / 3 > 0) ? 0 : (moveY * str / 3 < -50) ? -50 : moveY * str / 3
-            // $item_img.css('transition','0ms')
-            $item_img.css('transform', 'rotateY(' + imgRotate + 'deg) rotateX(' + moveX * str / -5 + 'deg)');
+            var rotaX = moveX * str / -2;
+            var rotaY = (moveY * str / 3 > 0) ? 0 : (moveY * str / 3 < -50) ? -50 : moveY * str / 3;
+            $item_img.css('transform', 'rotateY(' + rotaY + 'deg) rotateX(' + rotaX + 'deg)');
 
         }
 
     });
 
-    var $item_img = $('.m-carousel__item');
-    $item_img.on('mousemove mousein', function (e) {
-        // $item_img.css('transform', 'rotateY(' + imgRotate + 'deg) rotateX(' + moveX * str / 3 + 'deg)');
-    });
+
     $item_img.on('mouseleave', function (e) {
-        // $item_img.css('transition','200ms')
         $item_img.css('transform', 'rotateY(0deg) rotateX(0deg)');
     });
 });
